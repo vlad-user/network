a9e44301cef9aa98a17fc0df4c3b4f909e40538d
vlad-user <vladpush@campus.technion.ac.il>
2019-01-31 22:31:50 +0200

    fix

--- async_network.py
+++ async_network.py
@@ -3,8 +3,9 @@ import itertools
 import sys
 import numpy as np
 import math
-import  mpi4py
+import mpi4py
 from time import time
+
 mpi4py.rc(initialize=False, finalize=False)
 from mpi4py import MPI
 
@@ -12,11 +13,14 @@ from mpi4py import MPI
 class AsynchronicNeuralNetwork(NeuralNetwork):
 
     def __init__(self, sizes=list(), learning_rate=1.0, mini_batch_size=16, number_of_batches=16,
-                 epochs=10, number_of_masters = 1,  matmul=np.matmul):
-    # calling super constructor
+                 epochs=10, number_of_masters=1, matmul=np.matmul):
+        # calling super constructor
         super().__init__(sizes, learning_rate, mini_batch_size, number_of_batches, epochs, matmul)
-    # setting number of workers and masters
+        # setting number of masters
         self.num_masters = number_of_masters
+        # We want to divide the layers between the masters so let's define:
+        self.layers_per_master = self.num_layers / self.num_masters
+        # print("init done")
 
     def fit(self, training_data, validation_data=None):
         # MPI setup
@@ -25,131 +29,143 @@ class AsynchronicNeuralNetwork(NeuralNetwork):
         self.comm = MPI.COMM_WORLD
         self.rank = self.comm.Get_rank()
         self.size = self.comm.Get_size()
+
+        # setting number of workers
         self.num_workers = self.size - self.num_masters
 
-        
         self.layers_per_master = self.num_layers // self.num_masters
-        
+
         # split up work
+        # print("split up!")
         if self.rank < self.num_masters:
             self.do_master(validation_data)
         else:
             self.do_worker(training_data)
-        
+
         # when all is done
         self.comm.Barrier()
         MPI.Finalize()
-    
+
     def do_worker(self, training_data):
-    #worker functionality
-    
-    #Parameters
-    #    ----------
-    #training_data : a tuple of data and labels to train the NN with
-    
-        
-    # setting up the number of batches the worker should do every epoch
-        #TODO
-        # we want that worker i will do mini_batches[t] if t%i == 0.
-        my_worker_index = self.rank - self.num_masters # my index as if it was started from 0
-        for epoch in range(self.epochs):  
+        '''
+        worker functionality
+
+        Parameters
+            ----------
+        training_data : a tuple of data and labels to train the NN with
+        '''
+
+        # setting up the number of batches the worker should do every epoch
+        # TODO
+        # print("Worker " + str(self.rank))
+        for epoch in range(self.epochs):
             # creating batches for epoch
             data = training_data[0]
             labels = training_data[1]
-            mini_batches = self.create_batches(data, labels, self.mini_batch_size)
-            for idx in range(my_worker_index, self.number_of_batches, self.num_workers):
-                x, y = mini_batches[idx]
+            # We want all of the workers together to do number_of_batches batches. Currently every
+            # worker does that so we want every worker to do number_of_batches/number_of_workers batches
+            res_batches = self.create_batches(data, labels, self.mini_batch_size)
+            mini_batches = res_batches[0:int(self.number_of_batches / self.num_workers)]
+            num_layers_multiple = self.num_masters*self.num_layers
+            for x, y in mini_batches:
                 # do work - don't change this
                 self.forward_prop(x)
                 nabla_b, nabla_w = self.back_prop(y)
-                
-                # send nabla_b, nabla_w to masters 
-                #TODO
-                send_list = []
-                # the worker sends all his layers
-                for i in range(self.num_layers):
-                    send_list.append(self.comm.Isend(nabla_w[i], dest = i%self.num_masters))
-                    send_list.append(self.comm.Isend(nabla_b[i], dest = i%self.num_masters))
-                # for request in send_list:
-                #   MPI.Request.Wait(request)
-                # recieve new self.weight and self.biases values from masters
-                # TODO
-                recv_list = []
-                # the worker receives new weights & biases from all masters
-                for i in range(self.num_layers):
-                    recv_list.append(self.comm.Irecv(self.weights[i], source = i%self.num_masters))
-                    recv_list.append(self.comm.Irecv(self.biases[i], source = i%self.num_masters))
-                for request in recv_list:
-                    MPI.Request.Wait(request)
-                
-                             
+
+                # send nabla_b, nabla_w to masters
+                for i in range(0, self.num_layers):
+                    curr_master = i%self.num_masters
+                    self.comm.Isend(nabla_b[i], curr_master, 2*i)
+                    self.comm.Isend(nabla_w[i], curr_master, 2*i+1)
+
+                #We'll need to check which answeres did we get
+                recv_requests = []
+                for i in range(0, self.num_layers):
+                    expected_master =  i%self.num_masters
+                    request_bias   = self.comm.Irecv(self.biases[i], expected_master, 2*i)
+                    request_weight = self.comm.Irecv(self.weights[i], expected_master, 2*i+1)
+
+                    recv_requests.append(request_bias)
+                    recv_requests.append(request_weight)
+                for x in recv_requests:
+                    x.Wait()
+                #make sure it all came and start over
+
     def do_master(self, validation_data):
         ''' master functionality
-        
-    Parameters
+
+        Parameters
         ----------
-    validation_data : a tuple of data and labels to train the NN with
-    '''
-        
-    #setting up the layers this master does
+        validation_data : a tuple of data and labels to train the NN with
+        '''
+
+        # setting up the layers this master does
+        # print("Master " + str(self.rank))
         nabla_w = []
         nabla_b = []
         for i in range(self.rank, self.num_layers, self.num_masters):
             nabla_w.append(np.zeros_like(self.weights[i]))
             nabla_b.append(np.zeros_like(self.biases[i]))
-            
-        for epoch in range(self.epochs ):
+
+        for epoch in range(self.epochs):
             for batch in range(self.number_of_batches):
-                
-        # wait for any worker to finish batch and
-        # get the nabla_w, nabla_b for the master's layers
-        # TODO
-                recv_list = []
-                glob_status = MPI.Status()
-                # as we send the layers "sequentially", we first scan to the first location
-                # in nabla array , then according to the worker who sent the layers,
-                # we keep receiving the regarding layers
-                req_to_get_worker = self.comm.Irecv(nabla_w[0],source = MPI.ANY_SOURCE)
-                MPI.Request.Wait(req_to_get_worker, glob_status)
-                # now we can get the source and stuff
-                worker_to_receive_from = glob_status.Get_source()
-                #get biases for first place
-                req_to_get_worker2 = self.comm.Irecv(nabla_b[0],source=worker_to_receive_from)                
-                MPI.Request.Wait(req_to_get_worker2)
-                for i in range(1,len(nabla_w)):
-                    recv_list.append(self.comm.Irecv(nabla_w[i],source = worker_to_receive_from))
-                    recv_list.append(self.comm.Irecv(nabla_b[i],source = worker_to_receive_from))
-                for request in recv_list:
-                    MPI.Request.Wait(request)
+                # print("Batch number " + str(batch) + " in " + str(self.rank))
+
+                # wait for any worker to finish batch and
+                # get the nabla_w, nabla_b for the master's layers
+                status = MPI.Status()
+                worker_request = self.comm.Irecv(nabla_b[0], MPI.ANY_SOURCE, 2*self.rank)
+                worker_request.Wait(status)
+                worker = status.Get_source()
+                # print("Master " + str(self.rank) + " learned the source of " + str(worker))
+
+                recv_requests = []
+                for c, x in enumerate(range(self.rank, self.num_layers, self.num_masters)):
+                    if not c==0:
+                        request_bias = self.comm.Irecv(nabla_b[c], worker, 2*x)
+                        recv_requests.append(request_bias)
+                    request_weight = self.comm.Irecv(nabla_w[c], worker, 2 * x+1)
+                    recv_requests.append(request_weight)
+                # print("Master "+ str(self.rank) + " got all of the requests, now waiting")
+                for x in recv_requests:
+                    x.Wait()
+
+                # print("Master " + str(self.rank) + " is done waiting")
                 # calculate new weights and biases (of layers in charge)
                 for i, dw, db in zip(range(self.rank, self.num_layers, self.num_masters), nabla_w, nabla_b):
-                    self.weights[i] = self.weights[i] - self.eta *  dw
-                    self.biases[i] = self.biases[i] - self.eta *  db
-            
+                    self.weights[i] = self.weights[i] - self.eta * dw
+                    self.biases[i] = self.biases[i] - self.eta * db
+                # print("Master " + str(self.rank) + " TRAINED")
+
+                for c, x in enumerate(range(self.rank, self.num_layers, self.num_masters)):
+                    self.comm.Isend(self.biases[x], worker, 2*x)
+                    self.comm.Isend(self.weights[x], worker, 2*x+1)
                 # send new values (of layers in charge)
-                #TODO
-                send_list = []
+
+                # print("Master " + str(self.rank) + " sent back to worker " + str(worker))
+                nabla_w = []
+                nabla_b = []
                 for i in range(self.rank, self.num_layers, self.num_masters):
-                    send_list.append(self.comm.Isend(self.weights[i],dest = worker_to_receive_from))
-                    send_list.append(self.comm.Isend(self.biases[i],dest = worker_to_receive_from))
-                # for request in  send_list:
-                #   MPI.Request.Wait(request)
-                
+                    nabla_w.append(np.zeros_like(self.weights[i]))
+                    nabla_b.append(np.zeros_like(self.biases[i]))
+
+            # TODO
+
             self.print_progress(validation_data, epoch)
 
         # gather relevant weight and biases to process 0
-        #TODO
-        if self.rank != 0:
-            for i in range(self.rank, self.num_layers, self.num_masters):
-                req_w = self.comm.Isend(self.weights[i], dest = 0)
-                # MPI.Request.Wait(req_w)
-                req_b = self.comm.Isend(self.biases[i], dest = 0)
-                # MPI.Request.Wait(req_b)
-        if self.rank == 0:
-            recv_list = []
-            for master in range(1,self.num_masters):
-                for layer in range(master, self.num_layers,self.num_masters):
-                    recv_list.append(self.comm.Irecv(self.weights[layer],source = master))
-                    recv_list.append(self.comm.Irecv(self.biases[layer],source = master))
-            for request in recv_list:
-                MPI.Request.Wait(request)
+        if not self.rank==0:
+            for c, x in enumerate(range(self.rank, self.num_layers, self.num_masters)):
+                self.comm.Isend(self.biases[x], 0, 2 * x)
+                self.comm.Isend(self.weights[x], 0, 2 * x + 1)
+        else:
+            requests = []
+            for master in range(1, self.num_masters):
+                for x in range(master, self.num_layers, self.num_masters):
+                    master_bias_request = self.comm.Irecv(self.biases[x], master, 2 * x)
+                    master_weight_request = self.comm.Irecv(self.weights[x], master, 2 * x + 1)
+
+                    requests.append(master_bias_request)
+                    requests.append(master_weight_request)
+            for x in requests:
+                x.Wait()
