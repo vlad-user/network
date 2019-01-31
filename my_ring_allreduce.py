82bbeee0bb5098aa6173c744dea17bc0ec256aae
vlad-user <vladpush@campus.technion.ac.il>
2019-01-31 19:46:50 +0200

    fix

--- my_ring_allreduce.py
+++ my_ring_allreduce.py
@@ -37,7 +37,8 @@ def ringallreduce(send, recv, comm):
 
     for _ in range(comm.size - 1):
         #comm.Send([chunks[chunk2send], MPI.FLOAT], dest=next_pid)
-        
+        send_req = None
+        recv_req = None
         send_req = comm.Isend(chunks[chunk2send], dest=next_pid)
 
         tmp = np.zeros_like(chunks[chunk2recv])
@@ -57,20 +58,27 @@ def ringallreduce(send, recv, comm):
     chunk2recv = (chunk2send - 1) % size
 
     for _ in range(comm.size - 1):
-        #comm.send(chunks[chunk2send], dest=next_pid)
-        comm.Send([chunks[chunk2send], MPI.FLOAT], dest=next_pid)
-        #chunks[chunk2recv] = comm.recv(source=prev_pid)
+        send_req = None
+        recv_req = None
+        #comm.Send([chunks[chunk2send], MPI.FLOAT], dest=next_pid)
+        send_req = comm.Isend(chunks[chunk2send], dest=next_pid)
         tmp = np.zeros_like(chunks[chunk2recv])
-        comm.Recv([tmp, MPI.FLOAT], source=prev_pid)
+        #comm.Recv([tmp, MPI.FLOAT], source=prev_pid)
+        recv_req = comm.Irecv(tmp, source=prev_pid)
+        if send_req is not None:
+            send_req.wait()
+        if recv_req is not None:
+            recv_req.wait()
         np.copyto(chunks[chunk2recv], tmp)
         #chunks[chunk2recv] = tmp
         chunk2send = chunk2recv
         chunk2recv = (chunk2recv - 1) % size
     
-    recv_idx = 0
-    for chunk in chunks:
-        for i in range(chunk.shape[0]):
-            recv[recv_idx] = chunk[i]
-            recv_idx += 1
+    np.copyto(recv, np.concatenate(chunks, axis=0))
+    #recv_idx = 0
+    #for chunk in chunks:
+    #    for i in range(chunk.shape[0]):
+    #        recv[recv_idx] = chunk[i]
+    #        recv_idx += 1
     
     
